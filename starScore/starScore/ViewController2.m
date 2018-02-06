4af40b13bcfdc9095fabe0fcf8c84c8a1a2ebd78
zhouyu <1512450002@qq.com>
2018-02-06 14:53:16 +0800

    删除注释

--- starScore/starScore/ViewController2.m
+++ starScore/starScore/ViewController2.m
@@ -22,16 +22,7 @@
 
 - (void)demo {
     NSLog(@"测试输出");
+    NSLog(@"PDF");
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
