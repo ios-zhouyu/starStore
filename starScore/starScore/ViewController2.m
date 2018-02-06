039bb55b790ef0a9398667b6228009dcfe263d78
zhouyu <1512450002@qq.com>
2018-02-06 14:52:21 +0800

    增加测试方法

--- starScore/starScore/ViewController2.m
+++ starScore/starScore/ViewController2.m
@@ -20,9 +20,8 @@
     self.view.backgroundColor = [UIColor yellowColor];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)demo {
+    NSLog(@"测试输出");
 }
 
 /*
