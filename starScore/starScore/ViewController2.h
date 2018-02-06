c6140e2c40e2070d8d78edd723a527efc7d1152e
zhouyu <1512450002@qq.com>
2018-02-06 14:35:28 +0800

    新增viewController2

---
+++ starScore/starScore/ViewController2.h
@@ -0,0 +1,13 @@
+//
+//  ViewController2.h
+//  starScore
+//
+//  Created by zhouyu on 2018/2/6.
+//  Copyright © 2018年 star. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController2 : UIViewController
+
+@end
